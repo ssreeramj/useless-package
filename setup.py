9a608494802ca30040c3ec22a2ef48abb894957e
ssreeramj <ssreeramj@gmail.com>
2021-01-06 18:17:03 +0530

    second commit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Pretty much a useless package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ssreeramj/useless-package",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
